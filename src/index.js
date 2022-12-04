684c8357e59bf7622367bc8e10c7f2388b60a87c
Frank <frank@cocreate.app>
2022-12-04 12:34:30 +0000

    feat: replace mongodb with @cocreate/crud-server

--- src/index.js
+++ src/index.js
@@ -1,76 +1,87 @@
 const { parse } = require("node-html-parser");
-const { ObjectId } = require('mongodb');
 
-const renderedIgnoreEl = { INPUT: true, TEXTAREA: true, SELECT: true, LINK: true, IFRAME: true, "COCREATE-SELECT": true };
+class CoCreateServerSideRender {
+	constructor(crud) {
+		this.crud = crud;
+		this.renderedIgnoreEl = { INPUT: true, TEXTAREA: true, SELECT: true, LINK: true, IFRAME: true, "COCREATE-SELECT": true }
+	}
 
-module.exports = async function renderHtml(orgDB, html) {
+    async html(html) {
 
-    let dep = [];
-    let dbCache = new Map();
-
-    async function render(html, lastKey) {
-        const dom = parse(html);
-        for (let el of dom.querySelectorAll(
-                "[collection][name][document_id]"
-            )) {
-            let meta = el.attributes;
-
-            if (renderedIgnoreEl[el.tagName])
-                continue;
+        let dep = [];
+        let dbCache = new Map();
+    
+        async function render(html, lastKey) {
+            const dom = parse(html);
+            for (let el of dom.querySelectorAll(
+                    "[collection][name][document_id]"
+                )) {
+                let meta = el.attributes;
+    
+                if (renderedIgnoreEl[el.tagName])
+                    continue;
+                    
+                if (el.tagName == "DIV" && !el.classList.contains('domEditor'))
+                    continue;
+                    
+                if (el.classList.contains('domEditor') && el.closest('.template'))
+                    continue;
                 
-            if (el.tagName == "DIV" && !el.classList.contains('domEditor'))
-                continue;
-                
-            if (el.classList.contains('domEditor') && el.closest('.template'))
-                continue;
-           
-            if (el.hasAttribute('actions'))
-                continue;
-
-            let id = meta["document_id"],
-                coll = meta['collection'],
-                name = meta['name'];
-            let key = id + coll + name;
-            if (!id || !name || !coll) continue;
-            if (dep.includes(key))
-                throw new Error(
-                    `infinite loop: ${lastKey} ${id} ${coll} ${name}  has been already rendered`
-                );
-            else
-                dep.push(key)
-
-            let cacheKey = id + coll;
-            let record;
-            if (dbCache.has(cacheKey))
-                record = dbCache.get(cacheKey)
-            else {
-                let collection = orgDB.collection(coll)
-                record = await collection.findOne({"_id": new ObjectId(id)});
-                dbCache.set(cacheKey, record)
-            }
-
-            if (!record || !record[name]) {
+                if (el.hasAttribute('actions'))
+                    continue;
+    
+                let _id = meta["document_id"],
+                    collection = meta['collection'],
+                    name = meta['name'];
+                let key = _id + collection + name;
+                if (!_id || !name || !collection) continue;
+                if (dep.includes(key))
+                    throw new Error(
+                        `infinite loop: ${lastKey} ${_id} ${collection} ${name}  has been already rendered`
+                    );
+                else
+                    dep.push(key)
+    
+                let cacheKey = _id + collection;
+                let record;
+                if (dbCache.has(cacheKey))
+                    record = dbCache.get(cacheKey)
+                else {
+                    record = await this.crud.readDocument({
+                        collection,
+                        document: {
+                            _id
+                        }
+                    });
+                    record = record.document[0]
+                    dbCache.set(cacheKey, record)
+                }
+    
+                if (!record || !record[name]) {
+                    dep.pop();
+                    continue;
+                }
+                let chunk = record[name];
+                if (!chunk) {
+    
+                    dep.pop();
+                    continue;
+                }
+                let dom = await render(chunk);
+    
+                el.setAttribute('rendered', '')
+                el.innerHTML = "";
+                el.appendChild(dom);
+    
+    
                 dep.pop();
-                continue;
             }
-            let chunk = record[name];
-            if (!chunk) {
-
-                dep.pop();
-                continue;
-            }
-            let dom = await render(chunk);
-
-            el.setAttribute('rendered', '')
-            el.innerHTML = "";
-            el.appendChild(dom);
-
-
-            dep.pop();
+    
+            return dom;
         }
-
-        return dom;
+        let result = (await render(html, 'root')).toString();
+        return result;
     }
-    let result = (await render(html, 'root')).toString();
-    return result;
 }
+
+module.exports = CoCreateServerSideRender;

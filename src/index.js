5b2c4bcb3b56c2c802c4b0f7839a6e0110abd59b
frankpagan <frank@cocreate.app>
2023-07-27 14:26:08 -0600

    formating

--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@ const { parse } = require("node-html-parser");
 const { checkValue } = require("@cocreate/utils");
 
 class CoCreateServerSideRender {
-	constructor(crud) {
-		this.crud = crud;
-	}
+    constructor(crud) {
+        this.crud = crud;
+    }
 
     async HTML(html, organization_id) {
         const self = this
-		let ignoreElement = { INPUT: true, TEXTAREA: true, SELECT: true, LINK: true, IFRAME: true, "COCREATE-SELECT": true }
+        let ignoreElement = { INPUT: true, TEXTAREA: true, SELECT: true, LINK: true, IFRAME: true, "COCREATE-SELECT": true }
 
         let dep = [];
         let dbCache = new Map();
-    
+
         async function render(html, lastKey) {
             const dom = parse(html);
             for (let el of dom.querySelectorAll(
-                    "[collection][name][document_id]"
-                )) {
+                "[collection][name][document_id]"
+            )) {
                 let meta = el.attributes;
-    
+
                 if (ignoreElement[el.tagName])
                     continue;
-                    
+
                 if (el.tagName == "DIV" && !el.classList.contains('domEditor'))
                     continue;
-                    
+
                 if (el.classList.contains('domEditor') && el.closest('.template'))
                     continue;
-                
+
                 if (el.hasAttribute('actions'))
                     continue;
-    
+
                 let _id = meta["document_id"],
                     collection = meta['collection'],
                     name = meta['name'];
@@ -44,7 +44,7 @@ class CoCreateServerSideRender {
                     );
                 else
                     dep.push(key)
-    
+
                 let cacheKey = _id + collection;
                 let record;
                 if (dbCache.has(cacheKey))
@@ -59,10 +59,10 @@ class CoCreateServerSideRender {
                     });
                     if (record && record.document && record.document[0])
                         record = record.document[0]
-            
+
                     dbCache.set(cacheKey, record)
                 }
-    
+
                 if (!record || !record[name]) {
                     dep.pop();
                     continue;
@@ -73,15 +73,15 @@ class CoCreateServerSideRender {
                     continue;
                 }
                 let dom = await render(chunk);
-    
+
                 el.setAttribute('rendered', '')
                 el.innerHTML = "";
                 el.appendChild(dom);
-    
-    
+
+
                 dep.pop();
             }
-    
+
             return dom;
         }
         let result = await render(html, 'root');
